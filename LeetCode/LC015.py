c3b8c03497f462b9d8023a6de6532ed19b46f196
linhebin <linhebin@qiyi.com>
2020-04-29 16:39:21 +0800

    study

--- LeetCode/LC015.py
+++ LeetCode/LC015.py
@@ -18,21 +18,31 @@
 """
 class Solution:
     def threeSum(self, nums):
-        n = len(nums)
-        ls = []
-        for i in range(n-2):
-            print(i)
-            for j in range(i+1, n-1):
-                print(i, j)
-                for k in range(j+1, n):
-                    print(i, j, k)
-                    if nums[i] + nums[j] + nums[k] == 0:
-                        new_ls = sorted([nums[i], nums[j], nums[k]])
-                        print(new_ls)
-                        if new_ls not in ls:
-                            ls.append(new_ls)
-        return ls
+        res = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    while l < r and nums[l] == nums[l + 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r - 1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+        return res
+
 
 if __name__ == "__main__":
     S = Solution()
-    print(S.threeSum([0,0,0]))
+    print(S.threeSum([-1, 0, 1, 2, -1, -4]))
